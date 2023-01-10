510f0c3138aa059ab41855f9de35fc5ffabb9475
zlw <117007671@qq.com>
2023-01-10 15:11:49 +0800

    亚马逊产品名主图获取

--- src/main/java/com/zhz/selenium/controller/LcAmazonApiController.java
+++ src/main/java/com/zhz/selenium/controller/LcAmazonApiController.java
@@ -1,9 +1,18 @@
 package com.zhz.selenium.controller;
 
+import com.amazon.SellingPartnerAPIAA.AWSAuthenticationCredentials;
+import com.amazon.SellingPartnerAPIAA.AWSAuthenticationCredentialsProvider;
+import com.amazon.SellingPartnerAPIAA.LWAAuthorizationCredentials;
+import com.amazon.spapi.catalogItemsV0.ApiException;
+import com.amazon.spapi.catalogItemsV0.api.CatalogApi;
+import com.amazon.spapi.catalogItemsV0.model.GetCatalogItemResponse;
+
 import com.zhz.selenium.pojo.ApiResult;
 import com.zhz.selenium.service.LcAmazonApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +20,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/lcAmazonapi")
 public class LcAmazonApiController {
+
+    @Value("${USER_ACCESS_KEY_ID}")
+    private String accessKeyId;
+
+    @Value("${USER_SECRET_ACCESS_KEY}")
+    private String secretAccessKey;
+
+    @Value("${ROLE_ARN}")
+    private String roleArn;
+
+    @Value("${APP_CLIENT_ID}")
+    private String appClientId;
+
+    @Value("${APP_CLIENT_SECRET}")
+    private String appClientSecret;
+
+    @Value("${REFRESH_TOKEN}")
+    private String refreshToken;
+
+
+
 
     @Autowired
     private LcAmazonApiService lcAmazonApiService;
@@ -40,6 +71,9 @@ public class LcAmazonApiController {
             strList +=obj.getAmzAsin() + ","+obj.getZrPm()+","+obj.getGgPm()+";</br>";
         }
 
+        if(CollectionUtils.isEmpty(apiList)){
+            strList +=0 + ","+0+","+0+";</br>";
+        }
         System.out.println(strList);
 
         String strTitle ="ASIN,自然排名(最近30天),广告排名(最近30天)</br>";
@@ -64,7 +98,12 @@ public class LcAmazonApiController {
         ApiResult bean = lcAmazonApiService.selectSalesAsin(daterange, asin);
 
         String strList ="";
-        strList +=bean.getAsinSession() + ","+bean.getUnitOrder()+","+bean.getClicks()+","+bean.getSpend()+","+bean.getDay7TotalOrders();
+        if(bean!=null){
+            strList +=bean.getAsinSession() + ","+bean.getUnitOrder()+","+bean.getClicks()+","+bean.getSpend()+","+bean.getDay7TotalOrders();
+        }else{
+            strList +=asin + ","+0+","+0+","+0+","+0;
+        }
+
 
         String strTitle ="Session数(自然流量 sp api),总订单数(sp api),总广告点击数(该ASIN所在Campaign的),广告花费,广告订单数</br>";
 
@@ -82,6 +121,9 @@ public class LcAmazonApiController {
         for(ApiResult bean : list){
             strList +=bean.getCustomerSearchTerm() + ","+bean.getClicks()+","+bean.getCpc()+","+bean.getImpressions()+","+bean.getDay7TotalOrders()+";"+"</br>";
 
+        }
+        if(CollectionUtils.isEmpty(list)){
+            strList +=0+ ","+0+","+0+","+0+","+0+";"+"</br>";
         }
 
         String strTitle ="search term,点击数,CPC,曝光量,订单数</br>";
@@ -102,9 +144,68 @@ public class LcAmazonApiController {
 
         }
 
+        if(CollectionUtils.isEmpty(list)){
+            strList +=0 + ","+0+","+0+";"+"</br>";
+        }
+
         String strTitle ="search term,自然排名,广告排名</br>";
 
         return strTitle+strList;
+    }
+
+    @RequestMapping(value = "/listingDetail", method = RequestMethod.GET)
+    @ResponseBody
+    public String listingDetail(@RequestParam(value="asin") String asin) throws ApiException {
+
+        String strList ="";
+
+        String marketplaceId = "ATVPDKIKX0DER";
+//        String asin = "B09TT6BY6F";
+        CatalogApi api = api();
+        GetCatalogItemResponse response = api.getCatalogItem(marketplaceId, asin);
+
+        if(response!=null){
+            strList +=response.getPayload().getAttributeSets().get(0).getTitle() + "</br>"+response.getPayload().getAttributeSets().get(0).getSmallImage().getURL().replaceAll("._SL75_.jpg",".jpg");
+        }else{
+            strList +=0+","+0;
+        }
+
+        String strTitle ="产品标题,主图</br>";
+
+        return strTitle+strList;
+    }
+
+
+    public CatalogApi api(){
+        AWSAuthenticationCredentials awsAuthenticationCredentials = AWSAuthenticationCredentials.builder()
+                .accessKeyId(accessKeyId)
+                .secretKey(secretAccessKey)
+                .region("us-east-1")
+                .build();
+
+        AWSAuthenticationCredentialsProvider awsAuthenticationCredentialsProvider = AWSAuthenticationCredentialsProvider.builder()
+                .roleArn(roleArn)
+                .roleSessionName(getRandomNonce())
+                .build();
+
+        LWAAuthorizationCredentials lwaAuthorizationCredentials = LWAAuthorizationCredentials.builder()
+                .clientId(appClientId)
+                .clientSecret(appClientSecret)
+                .refreshToken(refreshToken)
+                .endpoint("https://api.amazon.com/auth/o2/token")
+                .build();
+
+        CatalogApi api = new CatalogApi.Builder().awsAuthenticationCredentials(awsAuthenticationCredentials)
+                .awsAuthenticationCredentialsProvider(awsAuthenticationCredentialsProvider)
+                .lwaAuthorizationCredentials(lwaAuthorizationCredentials)
+                .endpoint("https://sellingpartnerapi-na.amazon.com")//请求地区
+                .build();
+        return api;
+    }
+
+
+    public static String getRandomNonce() {
+        return UUID.randomUUID().toString().replace("-", "");
     }
 
 }
